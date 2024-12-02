64de11aa79fa483604dbf558b8267fcf523a1073
Awais Ishtiaq <144543611+AwaisIshtiaq@users.noreply.github.com>
2024-12-02 14:12:41 +0500

    Add files via upload

---
+++ variable_and_datatype.py
@@ -0,0 +1,102 @@
+# variables
+# numeric variables 
+
+# integer variable
+a = 10
+
+# float vaiable
+b = 2.6
+
+# string variable
+c = "triolinks"
+
+# boolean vaiable
+d = True
+
+# print all the statement
+print(a)
+print(b)
+print(c)
+print("\n")
+
+# Assign multiple values to multiple  variables
+
+x , y , z = 15, 20.2, "triolinks"
+
+
+# print all the statement
+print(x)
+print(y)
+print(z)
+
+print("\n")
+
+# print types of all the vaiables
+print(f"variable types: type of  variable a :{type(a)}, type of variable b : {type(b)}, and type of variable c : {type(c)}")
+print(f"variable types: type of  variable x: {type(x)}, type of  variable y :{type(y)}, type of  variable z : {type(z)}")
+
+
+# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+
+# data types
+# 1
+# string type
+
+string_type = "string type"
+
+print(type(string_type))
+
+print("\n")
+
+# 2
+# integer 
+integer = 90
+print(type(integer))
+
+print("\n")
+
+# 3
+# float 
+float_type  = 20.67
+print(type(float_type))
+
+print("\n")
+
+# 4
+# list 
+
+list_of_fruits = ["apple", "banana", "cherry"]
+print(type(list_of_fruits))
+print("\n")
+
+# 5
+# tuple 
+tup = ("apple", "banana", "cherry")
+print(type(tup))
+
+
+# 6
+# dictionary
+data = {
+   "name": "ali",
+    "age" : 25
+}
+
+print(type(data))
+print("\n")
+
+
+fruits_set = {
+    "apple", "banana", "cherry"
+}
+print(type(fruits_set))
+
+# 7
+# boolean
+
+t = True
+f = False
+
+print(type(t))
+print(type(f))
